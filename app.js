47f940d530b68acdbb1860692946978609bf9453
Andrew Tom Mathew <80047902+Fortnex@users.noreply.github.com>
2024-11-23 18:33:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const sideMenu = document.querySelector("aside");
+const profileBtn = document.querySelector("#profile-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+profileBtn.onclick = function() {
+    sideMenu.classList.toggle('active');
+}
+window.onscroll = () => {
+    sideMenu.classList.remove('active');
+    if (window.scrollY > 0) { document.querySelector('header').classList.add('active'); }
+    else { document.querySelector('header').classList.remove('active'); }
+}
+
+themeToggler.onclick = function() {
+    document.body.classList.toggle('dark-theme');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+}
+
+// below function is done by vaishnavi
+// function to check equality of passwords
+function checkEquality(formName, input1 = "password", input2 = "repeatPassword", alertWhen = "no match") {
+    var x = document.forms[formName][input1].value;
+    var y = document.forms[formName][input2].value;
+    if (x == y && alertWhen == "match") {
+        alert('New password must be different from the old password');
+        return false;
+    }
+    if (x != y && alertWhen == "no match") {
+        alert('Password re-entered does not match');
+        return false;
+    }
+    return true;
+}
